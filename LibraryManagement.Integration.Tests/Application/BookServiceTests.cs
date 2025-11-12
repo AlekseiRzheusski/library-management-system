6fdfb92074c505ef4339c6c184f5f20b163c41be
a.rzheussky <a.rzheussky@syberry.com>
2025-11-12 20:34:03 +0300

    test: add CreateBookAsync method tests

--- LibraryManagement.Integration.Tests/Application/BookServiceTests.cs
+++ LibraryManagement.Integration.Tests/Application/BookServiceTests.cs
@@ -2,6 +2,8 @@ using SimpleInjector.Lifestyles;
 
 using LibraryManagement.Application.Services.Interaces;
 using LibraryManagement.Integration.Tests.Fixtures;
+using LibraryManagement.Application.Services.DTOs.BookModels;
+using FluentValidation;
 
 namespace LibraryManagement.Integration.Tests.Application;
 
@@ -37,6 +39,57 @@ public class BookServiceTests : IClassFixture<SqliteTestDatabaseFixture>
             var result = await service.GetBookAsync(10);
 
             Assert.Null(result);
+        }
+    }
+
+    [Fact]
+    public async Task CreateBookAsync_WhenCreateBookCommandIsCorrect_ShouldAddAndReturnBookDto()
+    {
+        using (AsyncScopedLifestyle.BeginScope(_fixture.Container))
+        {
+            var service = _fixture.Container.GetInstance<IBookService>();
+
+            var createBookCommand = new CreateBookCommand
+            {
+                Title = "Dance with Dragons",
+                Isbn = "9780606321853",
+                Description = "In the aftermath of a colossal battle, Daenerys Targaryen rules with her three dragons as queen of a city built on dust and death. But Daenerys has thousands of enemies, and many have set out to find her. Fleeing from Westeros with a price on his head, Tyrion Lannister, too, is making his way east--with new allies who may not be the ragtag band they seem. And in the frozen north, Jon Snow confronts creatures from beyond the Wall of ice and stone, and powerful foes from within the Night's Watch. In a time of rising restlessness, the tides of destiny and politics lead a grand cast of outlaws and priests, soldiers and skinchangers, nobles and slaves, to the greatest dance of all.",
+                AuthorId = 4,
+                CategoryId = 6,
+                PublishedDate = "2013-10-29",
+                PageCount = 1152
+            };
+
+            var resultBookDto = await service.CreateBookAsync(createBookCommand);
+
+            Assert.NotNull(resultBookDto);
+            Assert.Equal(createBookCommand.Title, resultBookDto.Title);
+            Assert.Equal(createBookCommand.PublishedDate, resultBookDto.PublishedDate);
+        }
+    }
+
+    [Fact]
+    public async Task CreateBookAsync_WhenCreateBookCommandIsIncorrect_ShouldThrow()
+    {
+        using (AsyncScopedLifestyle.BeginScope(_fixture.Container))
+        {
+            var service = _fixture.Container.GetInstance<IBookService>();
+
+            var createBookCommand = new CreateBookCommand
+            {
+                Title = "Dance with Dragons",
+                Isbn = "9780606321853",
+                Description = "In the aftermath of a colossal battle, Daenerys Targaryen rules with her three dragons as queen of a city built on dust and death. But Daenerys has thousands of enemies, and many have set out to find her. Fleeing from Westeros with a price on his head, Tyrion Lannister, too, is making his way east--with new allies who may not be the ragtag band they seem. And in the frozen north, Jon Snow confronts creatures from beyond the Wall of ice and stone, and powerful foes from within the Night's Watch. In a time of rising restlessness, the tides of destiny and politics lead a grand cast of outlaws and priests, soldiers and skinchangers, nobles and slaves, to the greatest dance of all.",
+                AuthorId = 10,
+                CategoryId = 6,
+                PublishedDate = "10-10-10",
+                PageCount = 1152
+            };
+
+            await Assert.ThrowsAsync<ValidationException>(async () =>
+            {
+                var resultBookDto = await service.CreateBookAsync(createBookCommand);
+            });
         }
     }
 }
