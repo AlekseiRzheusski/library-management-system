7c77a2c9850bfb37f7e403a440b380cd206b15b9
a.rzheussky <a.rzheussky@syberry.com>
2025-11-19 20:04:45 +0300

    refactor: add missing namespace in ISearchService and _logger to interceptor

--- LibraryManagement.Api/Logging/LoggingInterceptor.cs
+++ LibraryManagement.Api/Logging/LoggingInterceptor.cs
@@ -4,22 +4,29 @@ using Serilog;
 
 public class LoggingInterceptor : Interceptor
 {
+    private readonly ILogger<LoggingInterceptor> _logger;
+
+    public LoggingInterceptor(ILogger<LoggingInterceptor> logger)
+    {
+        _logger = logger;
+    }
+
     public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(
         TRequest request,
         ServerCallContext context,
         UnaryServerMethod<TRequest, TResponse> continuation)
     {
-        Log.Information("gRPC Request: {Method}, Payload: {@Request}", context.Method, request);
+        _logger.LogInformation("gRPC Request: {Method}, Payload: {@Request}", context.Method, request);
 
         try
         {
             var response = await continuation(request, context);
-            Log.Information("gRPC Response: {@Response}", response);
+            _logger.LogInformation("gRPC Response: {@Response}", response);
             return response;
         }
         catch (Exception ex)
         {
-            Log.Error(ex, "gRPC Error: {Method}", context.Method);
+            _logger.LogError(ex, "gRPC Error: {Method}", context.Method);
             throw;
         }
     }
@@ -30,17 +37,17 @@ public class LoggingInterceptor : Interceptor
         ServerCallContext context,
         ServerStreamingServerMethod<TRequest, TResponse> continuation)
     {
-        Log.Information("gRPC ServerStreaming request: {Method}, Payload: {@Request}. Started...",
+        _logger.LogInformation("gRPC ServerStreaming request: {Method}, Payload: {@Request}. Started...",
             context.Method, request);
 
         try
         {
             await continuation(request, responseStream, context);
-            Log.Information("gRPC ServerStreaming method finished: {Method}", context.Method);
+            _logger.LogInformation("gRPC ServerStreaming method finished: {Method}", context.Method);
         }
         catch (Exception ex)
         {
-            Log.Error(ex, "gRPC ServerStreaming error: {Method}", context.Method);
+            _logger.LogError(ex, "gRPC ServerStreaming error: {Method}", context.Method);
             throw;
         }
     }
